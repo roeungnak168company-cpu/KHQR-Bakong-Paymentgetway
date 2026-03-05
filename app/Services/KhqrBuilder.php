1849ac041d00422c786890d53a53fa85fff647c8
roeungnak168company-cpu <roeungnak168.company@gmail.com>
2026-03-06 02:00:04 +0700

    KHQR-Bakong-Paymentgetway

--- app/Services/KhqrBuilder.php
+++ app/Services/KhqrBuilder.php
@@ -38,7 +38,7 @@ class KhqrBuilder
     }
 
     /**
-     * Build KHQR string matching Node's strict builder.
+     * Build Individual KHQR string (no tag 15 - matches official NBC KHQR SDK)
      * KHR only; amount must be integer.
      */
     public function buildKhqrString(array $input): string
@@ -58,29 +58,44 @@ class KhqrBuilder
         if (!preg_match('/^\d{13}$/', $createdMs)) throw new \InvalidArgumentException('createdAtMs must be 13 digits (ms epoch)');
         if (!preg_match('/^\d{13}$/', $expiredMs)) throw new \InvalidArgumentException('expiresAtMs must be 13 digits (ms epoch)');
 
+        // Tag 00: Payload format indicator
         $p00 = $this->tlv('00', '01');
-        $p01 = $this->tlv('01', '12');
-        $acquirerId = '1974011600520446BONG1000231208129291900';
-        $p15 = $this->tlv('15', '00' . str_pad((string)strlen($acquirerId), 2, '0', STR_PAD_LEFT) . $acquirerId);
 
+        // Tag 01: Point of initiation (12 = dynamic)
+        $p01 = $this->tlv('01', '12');
+
+        // Tag 29: Merchant account info (Individual KHQR - sub-tag 00 = bakong ID)
         $sub00 = '00' . str_pad((string)strlen($receiver), 2, '0', STR_PAD_LEFT) . $receiver;
         $p29 = $this->tlv('29', $sub00);
 
+        // Tag 52: Merchant category code
         $p52 = $this->tlv('52', '5999');
+
+        // Tag 53: Transaction currency (116 = KHR)
         $p53 = $this->tlv('53', '116');
+
+        // Tag 54: Transaction amount
         $p54 = $this->tlv('54', (string)((int)$amount));
+
+        // Tag 58: Country code
         $p58 = $this->tlv('58', 'KH');
+
+        // Tag 59: Merchant name
         $p59 = $this->tlv('59', substr($merchantName, 0, 25));
+
+        // Tag 60: Merchant city
         $p60 = $this->tlv('60', substr($merchantCity, 0, 15));
 
+        // Tag 99: Timestamp (created + expiry)
         $s00 = '00' . '13' . $createdMs;
         $s01 = '01' . '13' . $expiredMs;
         $p99 = $this->tlv('99', $s00 . $s01);
 
-        $withoutCrc = $p00 . $p01 . $p15 . $p29 . $p52 . $p53 . $p54 . $p58 . $p59 . $p60 . $p99 . '6304';
+        // Tag 63: CRC (always last)
+        $withoutCrc = $p00 . $p01 . $p29 . $p52 . $p53 . $p54 . $p58 . $p59 . $p60 . $p99 . '6304';
         $crc = $this->crc16ccittFalse($withoutCrc);
-        $p63 = '63' . '04' . $crc;
+        $p63 = '6304' . $crc;
 
-        return $p00 . $p01 . $p15 . $p29 . $p52 . $p53 . $p54 . $p58 . $p59 . $p60 . $p99 . $p63;
+        return $p00 . $p01 . $p29 . $p52 . $p53 . $p54 . $p58 . $p59 . $p60 . $p99 . $p63;
     }
 }
