d1f88903ed1a2da3ece976529be3c8395ea26c69
roeungnak168company-cpu <roeungnak168.company@gmail.com>
2026-03-05 20:07:18 +0700

    KHQR-Bakong-Paymentgetway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'api/qr/check',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
